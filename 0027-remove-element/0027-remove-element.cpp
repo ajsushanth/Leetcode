53a71ab6771aa9b637419555aff8b7d1e3e3af40
ajsushanth <140843511+ajsushanth@users.noreply.github.com>
2023-08-28 16:37:49 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.06%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int> arr;
+        // vector<int> arr;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] != val){
+        //         arr.push_back(nums[i]);
+        //     }
+        // }
+        // nums.clear();
+        // for(int i=0; i<arr.size(); i++){
+        //     nums.push_back(arr[i]);
+        // }
+        // return nums.size();
+
+        vector<int>::iterator p;
+        p = nums.begin();
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] != val){
-                arr.push_back(nums[i]);
+            if(nums[i] == val){
+                nums.erase(p); //removes the element at 'p' position and shifts all the elements towards left
+                p--;
+                i--;
             }
-        }
-        nums.clear();
-        for(int i=0; i<arr.size(); i++){
-            nums.push_back(arr[i]);
+            p++;
         }
         return nums.size();
     }
