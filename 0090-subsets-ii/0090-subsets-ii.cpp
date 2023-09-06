d7ca54a80e562c7d9c66add102a631c1e043dbd8
ajsushanth <140843511+ajsushanth@users.noreply.github.com>
2023-09-06 19:42:40 +0530

    Time: 6 ms (33.28%), Space: 7.8 MB (51.21%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        std::vector<std::vector<int>> ans;
+        // std::vector<std::vector<int>> ans;
+        vector<vector<int>> sol;
+        set<vector<int>> ans;
         vector<int> temp;
         sort(nums.begin(), nums.end());
         for(int i=0; i<pow(2,nums.size()); i++){
@@ -11,11 +13,15 @@ public:
                 k++;
                 j = j>>1;
             }
-            ans.push_back(temp);
+            ans.insert(temp);
             temp.clear();
         }
-        std::sort(ans.begin(), ans.end());
-        ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
-        return ans;
+        // std::sort(ans.begin(), ans.end());
+        // ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
+        for(auto ptr:ans){
+            sol.push_back(ptr);
+        }
+        return sol;
     }
 };
+// using set
