be3c52b021cf9d4698e8cd493b0cd41c2684b907
ajsushanth <140843511+ajsushanth@users.noreply.github.com>
2023-09-14 23:55:22 +0530

    Time: 148 ms (41.96%), Space: 67 MB (15.45%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -3,14 +3,14 @@ public:
     bool total_days(vector<int>& bloomDay, int days, int b, int f){
         int cf=0, boq=0;
         for(int i=0; i<bloomDay.size(); i++){
-            if(bloomDay[i] <= days) cf++;
+            if(bloomDay[i] <= days) cf++; //consecutive flowers
             else{
-                boq += cf/f;
+                boq += cf/f; //boquets possible
                 cf = 0;
             }
         }
         boq += cf/f;
-        if(boq >= b) return true;
+        if(boq >= b) return true; //required boquets are available
         else return false;
     }
 
@@ -19,13 +19,13 @@ public:
         if(b > bloomDay.size()/f) return -1;
         int min = INT_MAX, max = INT_MIN;
         for(int i=0; i<bloomDay.size(); i++){
-            if(min > bloomDay[i]) min = bloomDay[i];
-            if(max < bloomDay[i]) max = bloomDay[i];
+            if(min > bloomDay[i]) min = bloomDay[i]; //for atleast one flower to bloom
+            if(max < bloomDay[i]) max = bloomDay[i]; //for all the flowers to bloom
         }
         // cout << min << " " << max << endl;
         int l = min, h = max, ans = -1;
         while(l <= h){
-            int m = (l+h)/2;
+            int m = (l+h)/2; 
             bool ver = total_days(bloomDay, m, b, f);
             // cout << m << " " << ver << endl;
             if(ver){
