37edbdff25e886156dd6d05bda8b52f633126b8f
ajsushanth <140843511+ajsushanth@users.noreply.github.com>
2023-09-14 18:49:42 +0530

    Time: 0 ms (100%), Space: 14.1 MB (5.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,12 +3,12 @@ public:
     int maxProduct(vector<int>& nums) {
         int ans1 = nums[0], ans2 = nums[0], res = nums[0];
         for(int i=1; i<nums.size(); i++){
-            int temp = max({nums[i], ans1*nums[i], ans2*nums[i]});
-            ans2 = min({nums[i], ans1*nums[i], ans2*nums[i]});
+            int temp = max(nums[i], max(ans1*nums[i], ans2*nums[i]));
+            ans2 = min(nums[i], min(ans1*nums[i], ans2*nums[i]));
             ans1 = temp;
             res = max(res, ans1);
         }
         return res;
     }
-    //Kadane's Algorithm
 };
+// Kadane's Algorithm
