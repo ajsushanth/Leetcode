9e9bfdd6505d44f399aabba22c1e47f82c087df5
ajsushanth <140843511+ajsushanth@users.noreply.github.com>
2023-09-22 10:43:03 -0700

    Time: 112 ms (40.86%), Space: 58 MB (38.99%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -7,8 +7,8 @@ public:
         }
         for(int i=index; i<str.length(); i++){
             if(isPalindrome(str, index, i)){
-                // sub.push_back(str.substr(index, i-index+1));
-                sub.push_back(substr(str, index, i));
+                sub.push_back(str.substr(index, i-index+1));
+                // sub.push_back(substr(str, index, i));
                 solve(i+1, str, sub, ans);
                 sub.pop_back();
             }
@@ -24,13 +24,13 @@ public:
         return true;
     }
 // generating substring
-    string substr(string str, int start, int end){
-        string ans = "";
-        for(int i=start; i<=end; i++){
-            ans = ans + str[i];
-        }
-        return ans;
-    }
+    // string substr(string str, int start, int end){
+    //     string ans = "";
+    //     for(int i=start; i<=end; i++){
+    //         ans = ans + str[i];
+    //     }
+    //     return ans;
+    // }
 
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
